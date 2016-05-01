9279874689e733fbc77b075980e3a79c46d6f20d
Adrian Nuyda <adamnuyda@yahoo.com>
2016-05-01 12:42:23 -0700

    adds user model and championSchema

--- models/user.js
+++ models/user.js
@@ -1,10 +1,31 @@
 var mongoose = require('mongoose'),
     debug    = require('debug')('app:models');
 
-var userSchema = new mongoose.Schema({
-  name:   String,
-  handle: String
+championSchema = new mongoose.Schema({
+  name:             { type: String },
+  image:            { type: String }
 });
+
+var userSchema = new mongoose.Schema({
+  email:            { type: String, required: true, unique: true},
+  ign:              { type: String, required: true, unique: true},
+  primary_role:     { type: String },
+  transportation:   { type: Boolean },
+  location:         { type: String },
+  description:      { type: String },
+  team:             {
+                      type: mongoose.Schema.Types.ObjectId,
+                      ref: "User"
+                    },
+  profile_default:  { type: String },
+  tier:             { type: String },
+  division:         { type: String },
+  champion_pool:    [championSchema],
+  wins:             { type: Number },
+  losses:           { type: Number }
+});
+
+userSchema.plugin(require('mongoose-bcrypt'));
 
 var User = mongoose.model('User', userSchema);
 
