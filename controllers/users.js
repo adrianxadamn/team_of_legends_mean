96d69980aed42d2b95cc3c994abd0ea28ccf074c
Adrian Nuyda <adamnuyda@yahoo.com>
2016-05-02 21:22:39 -0700

    adds api call to request summonerId when creating a user. summonerId is now attached to a user's properties when created.

--- controllers/users.js
+++ controllers/users.js
@@ -1,31 +1,44 @@
 // Require resource's model(s).
+var request = require('request');
 var User = require("../models/user");
 
 function create(req, res, next) {
   if (!req.body.password) {
     return res.status(422).send('Missing required fields');
   }
-  User
-    .create(req.body)
-    .then(function(user) {
-      res.json({
-        success: true,
-        message: 'Successfully created user.',
-        data: {
-          email: user.email,
-          username:  user.username,
-          picture_url: user.picture_url,
-          id:    user._id
+
+  console.log("req.body:", req.body);
+
+  var baseUri = 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/';
+  var searchIgn = req.body.ign;
+  var apiKey = 'da00a520-06f1-4359-a686-4f48691f19a4';
+  var buildUri = baseUri + searchIgn + '?api_key=' + apiKey;
+  request.get(buildUri, function(err, data) {
+    var jsonData = JSON.parse(data.body);
+    console.log("jsonData:", jsonData);
+    console.log("summonerId:", jsonData[searchIgn].id);
+
+    User
+      .create({
+        email: req.body.email,
+        ign:   req.body.ign,
+        primary_role : req.body.primary_role,
+        summonerId: jsonData[searchIgn].id
+      })
+      .then(function(user) {
+        res.json({
+          success: true,
+          message: 'Successfully created user.'
+        });
+      }).catch(function(err) {
+        if (err.message.match(/E11000/)) {
+          err.status = 409;
+        } else {
+          err.status = 422;
         }
+        next(err);
       });
-    }).catch(function(err) {
-      if (err.message.match(/E11000/)) {
-        err.status = 409;
-      } else {
-        err.status = 422;
-      }
-      next(err);
-    });
+  });
 };
 
 
--- models/user.js
+++ models/user.js
@@ -9,20 +9,21 @@ championSchema = new mongoose.Schema({
 var userSchema = new mongoose.Schema({
   email:            { type: String, required: true, unique: true},
   ign:              { type: String, required: true, unique: true},
-  primary_role:     { type: String }
-  // transportation:   { type: Boolean },
-  // location:         { type: String },
-  // description:      { type: String },
-  // team:             {
-  //                     type: mongoose.Schema.Types.ObjectId,
-  //                     ref: "User"
-  //                   },
-  // profile_default:  { type: String },
-  // tier:             { type: String },
-  // division:         { type: String },
-  // champion_pool:    [championSchema],
-  // wins:             { type: Number },
-  // losses:           { type: Number }
+  primary_role:     { type: String },
+  transportation:   { type: Boolean },
+  location:         { type: String },
+  description:      { type: String },
+  team:             {
+                      type: mongoose.Schema.Types.ObjectId,
+                      ref: "User"
+                    },
+  profile_default:  { type: String },
+  tier:             { type: String },
+  division:         { type: String },
+  champion_pool:    [championSchema],
+  wins:             { type: Number },
+  losses:           { type: Number },
+  summonerId:       { type: String }
 });
 
 userSchema.plugin(require('mongoose-bcrypt'));
