2ced0961b031f5389a3ce6f51387e24006f35f3b
Adrian Nuyda <adamnuyda@yahoo.com>
2016-05-07 16:29:25 -0700

    add post model with postSchema and commentSchema

---
+++ models/post.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose'),
+    debug    = require('debug')('app:models');
+
+var commentSchema = new mongoose.Schema({
+  author:      {
+                 type: mongoose.Schema.Types.ObjectId,
+                 ref: "User"
+               },
+  body:        { type: String, required: true},
+  upvotes:     { type: Number, default: 0 },
+  createdAt:   { type: Date, default: Date.now }
+
+})
+
+var postSchema = new mongoose.Schema({
+  author:      {
+                 type: mongoose.Schema.Types.ObjectId,
+                 ref: "User"
+               },
+  title:       { type: String, required: true},
+  body:        { type: String, required: true},
+  comments:    [commentSchema],
+  upvotes:     { type: Number, default: 0 },
+  createdAt:   { type: Date, default: Date.now }
+
+
+});
+
+
+var Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
